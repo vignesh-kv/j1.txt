736f41b940ee64319ab4f352a045ac0c496e26b9
vignesh-kv <kv.vignesh2004@gmail.com>
2025-01-27 19:10:06 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class swap{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=5;
+int b=10;
+int c=a;
+a=b;
+b=c;
+System.out.println(a);
+System.out.println(b);
+}
+}
